c5781ce49024db1115ef0b2a5b3b19246c5c4ea2
ioane107 <eka.noniashvili.1974@gmail.com>
2024-10-31 15:27:12 +0400

    first commit

---
+++ day 03/homework/hw.py
@@ -0,0 +1,48 @@
+# N2
+
+print("ioane kopaliani"*100)
+
+# N3
+
+# კონკადინაცია არის ჯგუფური მუშაობა , რაზმის ლიდერსა და მოსწავლის ჯგუფური მუშაობა.
+
+# N4
+
+city = "London"
+print(city)
+
+# N5
+
+me = 11
+dad = 50
+mom = 50
+print(me + dad + mom) / 3
+
+# N6
+
+num1 = 6
+num2 = 4
+
+n1 = num1 ** 2
+n2 = num2 ** 2
+
+total = n1 + n2
+
+print(total)
+
+# N7
+
+nn1 = 3
+nn = 5
+
+print(nn1 + nn)
+print(nn1 - nn)
+print(nn1 * nn)
+print(nn1 / nn)
+print(nn1 % nn)
+
+# N8
+
+# რომ ვიმუშადოთ ჯგუფურად, და ჩვენმა ჯგუფის წევრმა რომ იცოდეს რა უნდა დაწეროს.
+
+
---
+++ day 04/homework/hw.py
@@ -0,0 +1,59 @@
+# N1
+
+points = 50
+points = 20
+points = 10
+
+print(points)
+
+# N2 
+
+# snake_case გამოიყენება ცვლადების სახელების  დასაწერად
+
+n_1 = "ioane"
+num_1 = 5
+number_4 = 1
+print(n_1,num_1,number_4)
+
+# N3
+# print(name')
+# print "country(
+# print "points'
+# print22()
+# print[;;o]
+# print{.a.}=
+# print)n(
+# print==num)
+# print(((s;)))
+# print()=math"
+# print(g%[()]
+      
+print("name")
+print("country")
+print("points")
+print(22)
+print("o")
+print("a")
+print("n")
+print("num")
+print("s")
+print("math")
+print("g")
+
+
+# N4
+
+age=11
+favnum=48
+num2 = age +favnum
+print(num2)
+
+# N5
+
+print(10 + 5)
+print(10 - 5) 
+print(10 * 5) 
+print(10 / 5) 
+print(10 % 5) 
+print(10 ** 5)
+print(10 // 5)
