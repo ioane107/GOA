2e06ff6cb40d2cffea5b600ddbbd5726885ea810
ioane107 <eka.noniashvili.1974@gmail.com>
2024-11-14 16:35:54 +0400

    second commit

--- day 06/homework/hw.py
+++ day 06/homework/hw.py
@@ -1,11 +1,11 @@
-1
+#1
 sport = input("please enter your favorite sport: ")
 print("your favorite sport is " + sport)
 
 #input ნიშნავს შეტანას
 #output ნიშნავს გამოტანას
 
-2
+#2
 name = input("please neter yor name:")
 surname = input("please neter yor surname:")
 age = input("please neter yor age:")
@@ -14,7 +14,7 @@ hobby = input("please neter yor hobby:")
 
 print("your name is " + name + ", your surname is " + surname + ", your age is " + age + ", your city is " + city + ", your hobby is " + hobby)
 
-3
+#3
 num1 = int(input("please enter first number:"))
 num2 = int(input("please enter seccond number:"))
 
