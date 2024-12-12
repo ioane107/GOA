b6e66ad3af0fcb42e4e93e025ebf04c9d54b4d92
ioane107 <eka.noniashvili.1974@gmail.com>
2024-12-12 17:15:49 +0400

    second commit

---
+++ day 10/classwork/cw.py
@@ -0,0 +1,12 @@
+print( 2 < 100 )
+print( 8 < 19 )
+print( 8 > 5 )
+print( 99 < 686 )
+print( 64 > 6 )
+
+
+print( 999 < 100 )
+print( 86 < 1 )
+print( 2 > 5 )
+print( 99 < 66 )
+print( 4 > 6 )
---
+++ day 10/homework/hw.py
@@ -0,0 +1,60 @@
+#n1
+#explisity არის მონაცემტა ტიპის გამიზნულად შეცვლა
+
+number = 8
+
+number = str(8)
+
+
+
+#2
+#implisity არის ავტომატურად მონაცემთა ტიპის შეცვლა ჩვენი მოქმედების გარეშე
+
+print(type(number))
+
+
+print(type(20 / 5)) # 4 --- 4.0
+
+
+
+
+
+
+
+
+
+
+
+
+print( 2 < 100 )
+print( 8 < 19 )
+print( 8 > 5 )
+print( 99 < 600 )
+print( 60 > 6 )
+print( 2 < 10 )
+print( 3 < 19 )
+print( 8 > 5 )
+print( 9 < 96 )
+print( 9 > 6 )
+
+
+
+
+
+
+print( 999 < 100 )
+print( 86 < 1 )
+print( 2 > 5 )
+print( 83 < 6 )
+print( 4 > 8 )
+print( 9 < 0 )
+print( 86 < 1 )
+print( 2 > 99 )
+print( 9 < 6 )
+print( 2 > 9 )
+
+
+
+
+
+
