20e6b81082a4c29f3a08305c999836205d1edb07
ioane107 <eka.noniashvili.1974@gmail.com>
2024-12-27 21:17:49 +0400

    second commit

---
+++ day 08/homework/hw.py
@@ -0,0 +1,38 @@
+
+#Integer (int) = 53
+#Float = 8.2
+#String (str) = "happy"
+#Boolean (bool) = true or false
+
+#2
+num1 = int(input("please enter your age:"))
+type (num1)
+
+
+
+
+#3
+num2 = int(input("please enter your age:"))
+print( num2 + 5 )
+
+
+#4
+name = input("please eter your name:")
+surname = input("please eter your surname:")
+age = input("please neter your age:")
+height = input("please enter your height")
+favcolor = input("please enter your fav color")
+
+print("your name is " + name + ", your surname is " + surname + ", your age is " + age + ", your height is " + height + ", your fav color is " + favcolor)
+
+
+
+
+
+
+
+
+
+
+
+
