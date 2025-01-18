d0fc16f95c8a9ac62ac04f00630cb9362c38c8a4
ioane107 <eka.noniashvili.1974@gmail.com>
2025-01-19 00:16:08 +0400

    second commit

---
+++ day 16/classwork/cw.py
@@ -0,0 +1,12 @@
+#1
+for i in range(50):
+    print(i)
+
+
+
+
+
+#2
+for i in range(10):
+    print("ioane")
+
---
+++ day 16/homework/hw3.py
@@ -0,0 +1,34 @@
+print(True or False) 
+a = 5 == 5 #true
+print(True or False) 
+b = 10 != 20 #true
+print(True or False) 
+c = 4 > 3 and 2 < 5 #true
+print(True or False) 
+g = 10 > 20 or 5 < 8 #true
+
+
+
+
+print(True or False) 
+print( 2 < 10 )
+print( 3 < 19 )
+print( 8 > 5 )
+print( 9 < 96 )
+print( 9 > 6 )
+
+
+
+
+print(True or False) 
+print( 2 <= 10 )
+print( 3 <= 19 )
+print( 8 >= 5 )
+print( 9 == 96 )
+print( 9 >= 6 )
+
+
+
+
+
+
