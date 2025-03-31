df0a1386e51a091b3e727e3fadb310cf28df20b2
ioane107 <eka.noniashvili.1974@gmail.com>
2025-03-31 22:46:08 +0400

    second commit

---
+++ day 26/cw.py
@@ -0,0 +1,22 @@
+#1
+surname = "კოპალიანი"
+first_letter = surname[0]
+last_letter = surname[-1]
+midle_letter = surname[4]
+print(first_letter)
+print(last_letter)
+print(midle_letter)
+
+
+#2
+misalmeba = "hello, vorld!"
+hello = misalmeba[:5]  
+world = misalmeba[7:] 
+print(hello)
+print(world)
+
+#3
+sentence = "practice makes perfect"
+reversed_sentence = sentence[::-1]
+print(sentence)
+print(reversed_sentence)
