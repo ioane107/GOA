1ef0a8260a68691e0cdd92758652e78e7267b54c
ioane107 <eka.noniashvili.1974@gmail.com>
2025-05-03 21:47:04 +0400

    second commit

---
+++ day 30.5/cw.py
@@ -0,0 +1,7 @@
+
+my_name = "ioane"
+user_name = input("neter yiur name: ")
+if my_name.lower() == user_name.lower():
+    print("our names ar similar !")
+else:
+    print("we have difernet names")
